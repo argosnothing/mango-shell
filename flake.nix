588a7dba1ac88d299eb3778d9ded02dbcd076128
salivala <argosnothing@gmail.com>
2025-09-27 10:15:34 -0400

    added flake

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "mango-shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    systems = [ "x86_64-linux" "aarch64-linux" ];
+    forAllSystems = f: builtins.listToAttrs (map (system: {
+      name = system;
+      value = f system;
+    }) systems);
+  in {
+    devShells = forAllSystems (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            quickshell          
+            wayland             
+            wl-clipboard        
+            grim slurp          
+            jq yq-go            
+          ];
+
+          shellHook = ''
+            export XDG_CONFIG_HOME="$PWD/.xdg"
+            mkdir -p "$XDG_CONFIG_HOME/quickshell"
+
+            echo "XDG_CONFIG_HOME is now: $XDG_CONFIG_HOME"
+            echo "Quickshell will read from ./.xdg/quickshell/"
+          '';
+        };
+      });
+  };
+}
+
